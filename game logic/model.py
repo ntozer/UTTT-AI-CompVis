2203fed82f45c989d9e6c7a5bdd4542d5626b854
ntozer <ntozer@unb.ca>
2018-06-07 23:31:50 -0300

    logic updates to engine

--- game logic/model.py
+++ game logic/model.py
@@ -2,8 +2,8 @@ from coord import Coord
 
 class Engine(object):
     def __init__(self):
-        self.board = [[0] * 9] * 9
-        self.master_board = [0] * 9 # contains the current state of all other boards
+        self.board = [[None] * 9] * 9
+        self.master_board = [None] * 9 # contains the win states of all other boards
         self.prev_move = Coord(None, None)
         self.player = 'X'
         self.game_state = 0
@@ -19,27 +19,30 @@ class Engine(object):
             self.update_game_state()
             # player updates
             self.update_player()
+            return True
+
+        return False
 
 
     @staticmethod
     def check_board_win(board):
         # checking verticals
         for idx in [0, 3, 6]:
-            if board[idx] == board[idx + 1]:
-                if board[idx] == board[idx + 2]:
+            if board[idx] is not None:
+                if board[idx] == board[idx + 1] and board[idx] == board[idx + 2]:
                     return True
         # checking horizontals
         for idx in [0, 1, 2]:
-            if board[idx] == board[idx + 3]:
-                if board[idx] == board[idx + 6]:
+            if board[idx] is not None:
+                if board[idx] == board[idx + 3] and board[idx] == board[idx + 6]:
                     return True
         # checking diagonals
-        if board[0] == board[4]:
-            if board[0] == board[8]:
-                return True
-        if board[2] == board[4]:
-            if board[2] == board[6]:
-                return True
+        if board[4] is not None:
+            if board[0] == board[4] and board[0] == board[8]:
+                    return True
+            if board[2] == board[4] and board[2] == board[6]:
+                    return True
+        return False
 
 
     def update_master_board(self):
@@ -62,10 +65,11 @@ class Engine(object):
     def check_valid_move(self, curr_move):
         if self.prev_move.x is not None:
             if self.prev_move.y != curr_move.x:
-                return False
+                if self.master_board[self.prev_move.y] is None:
+                    return False
 
         try:
-            if self.board[curr_move.x][curr_move.y] != 0:
+            if self.board[curr_move.x][curr_move.y] is not None:
                 return False
         except IndexError:
             return False
@@ -78,6 +82,3 @@ class Engine(object):
             self.player = 'O'
         else:
             self.player = 'X'
-
-
-
